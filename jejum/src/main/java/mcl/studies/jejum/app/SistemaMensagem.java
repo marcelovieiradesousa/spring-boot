116b9cae1702c897c4986d11f6a10dcad6b65e4a
Marcelo Vieira <marcelovieira.tec@gmail.com>
2023-01-25 14:31:16 -0300

    Add Configuration and Conf.Properties External

--- jejum/src/main/java/mcl/studies/jejum/app/SistemaMensagem.java
+++ jejum/src/main/java/mcl/studies/jejum/app/SistemaMensagem.java
@@ -1,28 +1,19 @@
 package mcl.studies.jejum.app;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SistemaMensagem implements CommandLineRunner {
-
-  @Value("${nome}")
-  private String nome;
-
-  @Value("${email}")
-  private String email;
-
-  @Value("${telefones}")
-  private List<Long> telefones;
+  @Autowired //Opera um instancia conectando ID
+  private Remetente remetente;
 
   @Override
   public void run(String... args) throws Exception {
-    System.out.println("Mensagem enviada por: " + nome
-        + "\nE-mail: " + email
-        + "\nCom telefones para contato: " + telefones);
+    System.out.println("Mensagem enviada por: " + remetente.getNome()
+        + "\nE-mail: " + remetente.getEmail()
+         + "\nCom telefones para contato: " + remetente.getTelefones());
     System.out.println("Seu cadastro foi efetuado!");
   }
 
