bc622c3c6da11fb69a45ca5f0e85ca57f73edfba
Marcelo Vieira <marcelovieira.tec@gmail.com>
2023-01-30 15:17:14 -0300

    Web API Rest, but all dont work out

---
+++ web/src/main/java/mcl/front/web/controller/UsuarioController.java
@@ -0,0 +1,37 @@
+package mcl.front.web.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import mcl.front.web.model.Usuario;
+import mcl.front.web.repository.UsuarioRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@RequestMapping("/users")
+public class UsuarioController {
+    @Autowired
+    private UsuarioRepository repository;
+    @GetMapping
+    public List<Usuario> list(){
+        return repository.findAll();
+    }
+    @PostMapping
+    public void save(@RequestBody Usuario user){
+        repository.save(user);
+    }
+    @PutMapping
+    public void update(@RequestBody Usuario user){
+        repository.save(user);
+    }
+    @GetMapping("/users/{username}")
+    public Usuario find(@PathVariable("/username") String username){
+        return repository.findByUsername(username);
+    }
+    @DeleteMapping("/users/{id}")
+    public void delete(@PathVariable("/id") Integer id){
+        repository.deleteById(id);
+    }
+}
---
+++ web/src/main/java/mcl/front/web/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package mcl.front.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+  @GetMapping("/")
+  public String welcome(){
+    return "Bem vindo garai";
+  }
+}
