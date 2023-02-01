b8626b1507f2c37fb9c2d0f48ca27c0ceb0c89b6
Marcelo Vieira <marcelovieira.tec@gmail.com>
2023-02-01 09:18:38 -0300

    Solve problem with compatibility version SB

--- web/src/main/java/mcl/front/web/doc/SwaggerConfig.java
+++ web/src/main/java/mcl/front/web/doc/SwaggerConfig.java
@@ -1,58 +1,32 @@
 package mcl.front.web.doc;
 
-import java.util.Arrays;
-import java.util.HashSet;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 @Configuration
 public class SwaggerConfig {
-  private Contact contato() {
-    return new Contact(
-        "Marcelo Vieira",
-        "https://www.linkedin.com/in/marcelo-vieira-sousa/",
-        "https://github.com/marcelovieiradesousa/");
-  }
-
-  private ApiInfoBuilder informacoesApi() {
-
-    ApiInfoBuilder apiInfoBuilder = new ApiInfoBuilder();
-
-    apiInfoBuilder.title("Minha API Documentada - REST API");
-    apiInfoBuilder.description("API exemplo de uso de Springboot REST API");
-    apiInfoBuilder.version("1.0");
-    apiInfoBuilder.termsOfServiceUrl("Termo de uso: Open Source");
-    apiInfoBuilder.license("Licença - Sua Empresa");
-    apiInfoBuilder.licenseUrl("http://www.seusite.com.br");
-    apiInfoBuilder.contact(this.contato());
-
-    return apiInfoBuilder;
-
-  }
-
   @Bean
-  public Docket detalheApi() {
-    Docket docket = new Docket(DocumentationType.SWAGGER_2);
-
-    docket
-        .select()
-        .apis(RequestHandlerSelectors.basePackage("mcl.front.web.controller")) // pacote.com.seus.controllers
-        .paths(PathSelectors.any())
-        .build()
-        .apiInfo(this.informacoesApi().build())
-        .consumes(new HashSet<String>(Arrays.asList("application/json")))
-        .produces(new HashSet<String>(Arrays.asList("application/json")));
-
-    return docket;
+  public OpenAPI springBlogPessoalOpenAPI() {
+    return new OpenAPI()
+        .info(new Info()
+            .title("Primeiro Projeto com Swagger")
+            .description(" - Documentando API Rest com Swagger usando Spring Boot Framework 3.0.2")
+            .version("v0.0.1")
+            .license(new License()
+                .name("Web Project with Spring Boot Web")
+                .url("https://github.com/marcelovieiradesousa/spring-boot"))
+            .contact(new Contact()
+                .name("Marcelo Vieira")
+                .url("https://www.linkedin.com/in/marcelo-vieira-sousa/")
+                .email("marcelovieira.tec@gmail.com")))
+        .externalDocs(new ExternalDocumentation()
+            .description("My Github")
+            .url("https://github.com/marcelovieiradesousa"));
   }
 }
